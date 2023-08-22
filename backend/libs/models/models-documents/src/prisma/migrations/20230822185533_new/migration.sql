91295ee142096f9b2d759ddf5fdf3425256d4b93
IsaijaMonk <isaijamonk@gmail.com>
2023-08-22 23:56:06 +0500

    Prisma

---
+++ backend/libs/models/models-documents/src/prisma/migrations/20230822185533_new/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "sellers_details" ADD COLUMN     "commentFD" TEXT NOT NULL DEFAULT '';
---
+++ backend/libs/models/models-documents/src/prisma/migrations/20230822185545_new/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `commentFD` on the `sellers_details` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "sellers_details" DROP COLUMN "commentFD";
