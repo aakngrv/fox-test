6b9431a767439a249d7748200a4be8bbf336b17d
Isaijah <aakngrv@mail.ru>
2023-08-30 12:54:54 +0500

    Заготовки интерфейсов

---
+++ backend/libs/shared/shared-types/src/lib/documents.interface.ts
@@ -0,0 +1,10 @@
+export interface SellerDetails {
+  id?: number;
+  name: string;
+  phone: string;
+  carCost: string;
+  role: boolean;
+  comment: string;
+  createdAt: Date;
+  publishAt: Date;
+}
