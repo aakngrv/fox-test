78dff5a62245c37a784edffb96671ccf9a326ee7
Isaijah <aakngrv@mail.ru>
2023-09-04 13:48:27 +0500

    +

--- backend/apps/betcar/src/app/prisma/prisma.service.ts
+++ backend/apps/betcar/src/app/prisma/prisma.service.ts
@@ -1,16 +1,9 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  post: any;
   async onModuleInit() {
     await this.$connect();
-  }
-
-  async enableShutdownHooks(app: INestApplication) {
-    this.$on('beforeExit', async () => {
-      await app.close();
-    })
   }
 }
