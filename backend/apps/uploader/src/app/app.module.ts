7cc340972632ce274a158a11b02a09f3398f9143
Isaijah <aakngrv@mail.ru>
2023-09-27 16:47:45 +0500

    uploader

--- backend/apps/uploader/src/app/app.module.ts
+++ backend/apps/uploader/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { FileModule } from './file/file.module';
+import { ConfigUploaderModule } from '@backend/config/config-uploader';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    FileModule,
+    ConfigUploaderModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
