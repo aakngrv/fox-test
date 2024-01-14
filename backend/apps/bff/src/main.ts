c276e2a3957e3b0f1d2cf33b5ad8f4505f787a5f
Isaijah <aakngrv@mail.ru>
2024-01-14 21:38:46 +0500

    Правки докерфайлов с структур

--- backend/apps/bff/src/main.ts
+++ backend/apps/bff/src/main.ts
@@ -1,14 +1,27 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 import { RequestIdInterceptor } from './app/interceptors/request-id.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const config = new DocumentBuilder()
+  .setTitle('The «Executors» service')
+  .setDescription('Executors service API')
+  .setVersion('1.0')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('users-spec', app, document);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalInterceptors(new RequestIdInterceptor());
+
+
   
   const port = process.env.PORT || 7000;
   await app.listen(port);
