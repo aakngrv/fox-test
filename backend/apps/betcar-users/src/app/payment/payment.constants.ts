00b56231e4a09bcde56989419e4ed6e8703923b9
Noa <aakngrv@mail.ru>
2025-01-21 18:44:52 +0500

    payment config

---
+++ backend/apps/betcar-users/src/app/payment/payment.constants.ts
@@ -0,0 +1,4 @@
+export const SHOP_ID = '990633';
+export const SHOP_PASSWORD = 'live_DrxUqOiVxN7tMlZJ6A9MGNHVZqa3eG7ywD3v_DLn1zM';
+export const PAYMENT_URL = `https://api.yookassa.ru/v3/payments`;
+export const RETURN_URL = "https://www.betcar.ru/main";
--- backend/apps/betcar-users/src/app/payment/payment.service.ts
+++ backend/apps/betcar-users/src/app/payment/payment.service.ts
@@ -1,122 +1,64 @@
 import { Injectable } from '@nestjs/common';
 import axios  from 'axios';
 import { v4 as uuidv4 } from 'uuid';
+import { PAYMENT_URL, RETURN_URL, SHOP_ID, SHOP_PASSWORD } from './payment.constants';
 
-let a;
 @Injectable()
 export class PaymentService {
-   
-  public paymentLink: string;
 
-  // public async createPayment() {
+  public getPaymentLink = async () => {
 
-    // const idempotenceKey = uuidv4();
+    const idempotenceKey = uuidv4();
 
-
-    public getPaymentLink = async () => {
-      const idempotenceKey = uuidv4();
-      try {
-        const response = await axios.post(
-          `https://api.yookassa.ru/v3/payments`,
-          {
-            amount: {
-              value: "2.00",
-              currency: "RUB"
-            },
-            confirmation: {
-              type: "redirect",
-              return_url: "https://www.betcar.ru/main"
-            },
-            capture: true,
-            description: "Покупка услуги",
-            receipt: {
-              customer: {
-                email: "aakngrv@mail.ru"
-              },
-              items: [
-                {
-                  description: "Покупка услуги",
-                  quantity: "1.000",
-                  amount: {
-                    value: "2.00",
-                    currency: "RUB"
-                  },
-                  vat_code: 1,
-                  payment_mode: "full_prepayment",
-                  payment_subject: "commodity",
-                  measure: "piece"
-                }
-              ]
-            }
+    try {
+      const response = await axios.post(
+        PAYMENT_URL,
+        {
+          amount: {
+            value: "2.00",
+            currency: "RUB"
           },
-          {
-            headers: {
-              "Content-Type": 'application/json',
-              "Idempotence-Key": idempotenceKey
-            },
-            auth: {
-              username: '990633',
-              password: 'live_DrxUqOiVxN7tMlZJ6A9MGNHVZqa3eG7ywD3v_DLn1zM'
-            },
+          confirmation: {
+            type: "redirect",
+            return_url: RETURN_URL
           },
-          )
-        return response.data.confirmation.confirmation_url
-      } catch (err) {
-        console.error(err.toJSON())
-      }
+          capture: true,
+          description: "Покупка услуги",
+          receipt: {
+            customer: {
+              email: "aakngrv@mail.ru"
+            },
+            items: [
+              {
+                description: "Покупка услуги",
+                quantity: "1.000",
+                amount: {
+                  value: "2.00",
+                  currency: "RUB"
+                },
+                vat_code: 1,
+                payment_mode: "full_prepayment",
+                payment_subject: "commodity",
+                measure: "piece"
+              }
+            ]
+          }
+        },
+        {
+          headers: {
+            "Content-Type": 'application/json',
+            "Idempotence-Key": idempotenceKey
+          },
+          auth: {
+            username: SHOP_ID,
+            password: SHOP_PASSWORD
+          },
+        },
+      )
+      return response.data.confirmation.confirmation_url;
+    } catch (err) {
+      console.error(err)
     }
-
-  //   axios
-  //     .post(
-  //       'https://api.yookassa.ru/v3/payments',
-  //       {
-  //         amount: {
-  //           value: "2.00",
-  //           currency: "RUB"
-  //         },
-  //         confirmation: {
-  //           type: "redirect",
-  //           return_url: "https://www.betcar.ru/main"
-  //         },
-  //         capture: true,
-  //         description: "Покупка услуги",
-  //         receipt: {
-  //           customer: {
-  //             email: "aakngrv@mail.ru"
-  //           },
-  //           items: [
-  //             {
-  //               description: "Покупка услуги",
-  //               quantity: "1.000",
-  //               amount: {
-  //                 value: "2.00",
-  //                 currency: "RUB"
-  //               },
-  //               vat_code: 1,
-  //               payment_mode: "full_prepayment",
-  //               payment_subject: "commodity",
-  //               measure: "piece"
-  //             }
-  //           ]
-  //         }
-  //       },
-  //       {
-  //         headers: {
-  //           "Content-Type": 'application/json',
-  //           "Idempotence-Key": idempotenceKey
-  //         },
-  //         auth: {
-  //           username: '990633',
-  //           password: 'live_DrxUqOiVxN7tMlZJ6A9MGNHVZqa3eG7ywD3v_DLn1zM'
-  //         },
-  //       },
-  //     )
-  //     .then((response) => {
-  //       this.paymentLink = (response.data.confirmation.confirmation_url);
-  //       a = this.paymentLink;
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     })
   }
-// }
+}
+
