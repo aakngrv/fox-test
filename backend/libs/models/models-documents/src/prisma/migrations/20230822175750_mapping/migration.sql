c1cb5bc546c4519301f8d030045cffca9fa72e78
Noa <kungurovss@gmail.com>
2025-11-25 18:04:57 +0500

    init front

--- backend/libs/models/models-documents/src/prisma/migrations/20230822175750_mapping/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `carId` on the `sellers_details` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "sellers_details" DROP COLUMN "carId",
-ADD COLUMN     "car_id" INTEGER;
